641a87d3b24f033d76db020c59d36c6649cdd457
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2022-07-22 17:46:24 +0200

    v0.0.1

---
+++ Formula/dean.rb
@@ -0,0 +1,18 @@
+class Dean < Formula
+    desc "Static DEpendency ANalyzer"
+    homepage "https://github.com/StaticDependencyAnalyzer/dean"
+    url "https://github.com/StaticDependencyAnalyzer/dean/archive/refs/tags/v0.0.1.tar.gz"
+    sha256 "692ba76076dd7272e1f4101a0539eb15379b6a083a259dce9c3de6d544ff8524"
+    license "AGPL-3.0"
+  
+    depends_on "rust" => :build 
+    
+    def install
+        system "cargo", "install", "--features", "no-self-update", *std_cargo_args  
+    end
+  
+    test do
+      system "true"
+    end
+  end
+  
